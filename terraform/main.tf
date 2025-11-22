5d6e955988a01290f78806aec3c9e0ad8db44583
achyuth94 <41371307+achyuth94@users.noreply.github.com>
2025-11-22 12:21:02 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,78 @@
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  number  = true
+  special = false
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+}
+
+# simple ACR
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard"
+  admin_enabled       = false
+}
+
+# Log Analytics
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "${var.prefix}-law-${random_string.suffix.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+# AKS
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "${var.prefix}-aks"
+
+  default_node_pool {
+    name       = "agentpool"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+    availability_zones = ["1", "2"]
+  }
+
+  identity { type = "SystemAssigned" }
+
+  addon_profile {
+    oms_agent {
+      enabled                    = true
+      log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+    }
+  }
+
+  depends_on = [azurerm_container_registry.acr]
+}
+
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+}
+
+# Key Vault
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "kv" {
+  name                        = var.keyvault_name
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+  soft_delete_enabled         = true
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+    secret_permissions = ["get", "list"]
+  }
+}
